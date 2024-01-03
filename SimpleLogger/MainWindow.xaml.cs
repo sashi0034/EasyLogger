61226105b049d14189026db0c3b222ce48bcd073
sashi0034 <satoshi3104.nishi@gmail.com>
2024-01-04 08:20:40 +0900

    最小限の構成を設定 🧰

--- SimpleLogger/MainWindow.xaml.cs
+++ SimpleLogger/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel;
+using System.IO;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SimpleLogger
 {
@@ -20,9 +14,49 @@ namespace SimpleLogger
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly CancellationTokenSource _cancellation = new();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            connectToPipe(_cancellation.Token);
+        }
+
+        public void addLogging(string text)
+        {
+            var textBox = new TextBox()
+            {
+                Text = text,
+                IsReadOnly = true,
+                TextWrapping = TextWrapping.Wrap,
+                Margin = new Thickness(8, 2, 8, 2),
+            };
+            stackPanel.Children.Add(textBox);
+        }
+
+        private async Task connectToPipe(CancellationToken cancellation)
+        {
+            addLogging("Waiting...");
+            using StreamReader reader = new StreamReader(Console.OpenStandardInput(), Encoding.UTF8);
+            while (cancellation.IsCancellationRequested == false)
+            {
+                var input = await reader.ReadLineAsync(cancellation);
+                // addLogging("🗨️ input");
+                if (input == null)
+                {
+                    // addLogging("😢 null");
+                    await Task.Delay(1000, cancellation);
+                    continue;
+                }
+
+                Dispatcher.Invoke(() => { addLogging("📝 " + input); });
+            }
+        }
+
+        private void onClosing(object? sender, CancelEventArgs e)
+        {
+            _cancellation.Cancel();
         }
     }
 }
